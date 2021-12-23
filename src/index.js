d730f0a1fae36b4954fa76be95da4efeb7f4c1eb
romanchenko2804 <romanchenko2804@gmail.com>
2021-12-24 00:12:41 +0200

    with prettier

--- src/index.js
+++ src/index.js
@@ -32,17 +32,17 @@ function renderCountriesInfo(countries) {
     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
     refs.countryList.innerHTML = '';
   }
-  
-    const markup = countries
-      .map(({ name, capital, population, flags, languages }) => {
-        return `<img src="${flags.svg}" alt="${name.official}" width="30px">
+
+  const markup = countries
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<img src="${flags.svg}" alt="${name.official}" width="30px">
           <h1 class="official-name">${name.official}</h1>
           <p><b>Capital:</b> ${capital}</p>
           <p><b>Population:</b> ${population}</p>
           <p><b>Langueges:</b> ${Object.values(languages)}</p>`;
-      })
-      .join('');
-    refs.countryInfo.innerHTML = markup;
+    })
+    .join('');
+  refs.countryInfo.innerHTML = markup;
 
   if (countries.length > 1) {
     refs.countryInfo.innerHTML = '';
